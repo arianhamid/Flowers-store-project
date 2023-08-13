3e979e495a14ddce551dfebae72509b29b8dab46
Hamid <hamidarianyegane@gmail.com>
2023-08-13 22:49:45 +0330

    styled nav component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ButtonContainer } from "./Button";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-sm  bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm  bg-primary navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
         Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk */}
@@ -16,7 +16,7 @@ const Navbar = () => {
         <ul className="navbar-nav align-item-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
-              Products
+              products
             </Link>
           </li>
         </ul>
@@ -28,10 +28,19 @@ const Navbar = () => {
             my cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
       <Outlet />
     </>
   );
 };
+
+// styled nav component
+const NavWrapper = styled.nav`
+background: var(--mainBlue) !important;
+.nav-link{
+color: var(--mainWhite) !important;
+font-size: 1.3rem;
+text-transform: capitalize;
+`;
 
 export default Navbar;
